99a2a017e8e342f51611fae462bb00894dc02679
Eric Nichols <linuxfreakus@gmail.com>
2018-07-17 10:24:13 -0700

    improve prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-SDK_ZIP=FitSDKRelease_20.66.00.zip
-SDK_DIR=FitSDKRelease_20.66.00
-mkdir -p downloads
-curl https://www.thisisant.com/assets/resources/FIT/$SDK_ZIP --output downloads/$SDK_ZIP
-unzip downloads/$SDK_ZIP -d downloads
-shopt -s dotglob && cp -a downloads/$SDK_DIR/* .
-rm -rf downloads/$SDK_DIR
+#
+# google docet - jump within the directory which contains this script
+#
+
+SOURCE="${BASH_SOURCE[0]}"
+
+while [ -h "$SOURCE" ]; do
+    DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+
+#
+# set aside the base dir for future references
+#
+
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+cd $DIR
+
+DOWNLOAD_DIR=downloads
+SDK=FitSDKRelease_20.66.00
+SDK_ZIP=$SDK.zip
+DOWNLOADED_ZIP=$DOWNLOAD_DIR/$SDK_ZIP
+EXTRACTED_CONTENTS=$DOWNLOAD_DIR/$SDK
+mkdir -p $DOWNLOAD_DIR
+if [ ! -f $DOWNLOADED_ZIP ]; then
+    curl https://www.thisisant.com/assets/resources/FIT/$SDK_ZIP --output $DOWNLOADED_ZIP
+fi
+unzip $DOWNLOADED_ZIP -d $DOWNLOAD_DIR
+shopt -s dotglob && cp -a $EXTRACTED_CONTENTS/* .
+rm -rf $EXTRACTED_CONTENTS
