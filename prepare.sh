6ab4d71361001807d096245b9ad6bc6dc8f2695c
Eric Nichols <linuxfreakus@gmail.com>
2018-07-17 10:33:45 -0700

    add more comments to prepare script

--- prepare.sh
+++ prepare.sh
@@ -16,16 +16,28 @@ done
 #
 
 DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+
+#
+# change to directory where script lives in case called from elsewhere
+#
 cd $DIR
 
-DOWNLOAD_DIR=downloads
+#
+# Choose SDK Version
+#
 SDK=FitSDKRelease_20.66.00
+
+#
+# Download and extract the sources
+#
+DOWNLOAD_DIR=downloads
 SDK_ZIP=$SDK.zip
+SDK_URL=https://www.thisisant.com/assets/resources/FIT/$SDK_ZIP
 DOWNLOADED_ZIP=$DOWNLOAD_DIR/$SDK_ZIP
 EXTRACTED_CONTENTS=$DOWNLOAD_DIR/$SDK
 mkdir -p $DOWNLOAD_DIR
 if [ ! -f $DOWNLOADED_ZIP ]; then
-    curl https://www.thisisant.com/assets/resources/FIT/$SDK_ZIP --output $DOWNLOADED_ZIP
+    curl $SDK_URL --output $DOWNLOADED_ZIP
 fi
 unzip $DOWNLOADED_ZIP -d $DOWNLOAD_DIR
 shopt -s dotglob && cp -a $EXTRACTED_CONTENTS/* .
